703e61cf15a259b37bdabd15477ddd41923dff61
pcatalini <pedro.catalini@hotmail.com>
2019-01-07 17:00:17 -0200

    comecando a ctriar integracao com swapi

---
+++ src/main/java/com/example/demo/service/SWAPIService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class SWAPIService {
+
+    private void callRestService(){
+        RestTemplate restTamplate = new RestTemplate();
+        //restTamplate.get
+    }
+
+}
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -18,4 +18,6 @@ public class UserService {
 	public List<User> getListOfUsers(){
 		return userRepository.findAll();
 	}
+
+
 }
